f92daabb2699f3e6934dd880bb5b0cf835d1e4d2
Gabriel <gabriel.brito@acqio.com.br>
2024-07-02 21:13:25 -0300

    refactor: TodoMainV2

--- src/domainV2/todo/main.ts
+++ src/domainV2/todo/main.ts
@@ -1,5 +1,6 @@
 import { ITodoItem } from "../global/types/itemTodo";
 import { NewTodoType } from "../global/types/newTodo";
+import { ITypeMessage } from "../global/types/typeMessage";
 import { AddTodoSub } from "./sub/addTodo/mainSub";
 import { GetListTodoSub } from "./sub/getListTodo/mainSub";
 import { RemoveTodoSub } from "./sub/removeTodo/mainSub";
@@ -12,13 +13,33 @@ type IConstructor = {
   removeTodoSub: RemoveTodoSub;
 };
 
+type IReturnDefault = Promise<{
+  data: {
+    listTodo: ITodoItem[];
+    allTodoCompleted: number;
+    quantityTodoCreated: number;
+  };
+  typeMessage: ITypeMessage;
+  message: string;
+}>;
+
+type IDataTodo = {
+  listTodo: ITodoItem[];
+  allTodoCompleted: number;
+  quantityTodoCreated: number;
+};
+
 export class TodoMainV2 {
   private getListTodoSub = new GetListTodoSub();
   private toggleDoneSub = new ToggleDoneSub();
   private addTodoSub = new AddTodoSub();
   private removeTodoSub = new RemoveTodoSub();
 
-  private listTodo: ITodoItem[] = [];
+  private dataTodo: IDataTodo = {
+    listTodo: [],
+    allTodoCompleted: 0,
+    quantityTodoCreated: 0,
+  };
 
   constructor(props?: IConstructor) {
     props?.getListTodoSub && (this.getListTodoSub = props.getListTodoSub);
@@ -27,24 +48,54 @@ export class TodoMainV2 {
     props?.removeTodoSub && (this.removeTodoSub = props.removeTodoSub);
   }
 
-  async getListTodo() {
+  async getListTodo(): IReturnDefault {
     const result = await this.getListTodoSub.execute();
-    this.listTodo = result.data.listTodo;
+    this.dataTodo = result.data;
     return result;
   }
 
-  async toggleDone(currentTodo: ITodoItem) {
-    await this.toggleDoneSub.execute(currentTodo);
+  async toggleDone(currentTodo: ITodoItem): IReturnDefault {
+    const result = await this.toggleDoneSub.execute(currentTodo);
+
+    if (result.typeMessage !== ITypeMessage.SUCCESS) {
+      return {
+        data: this.dataTodo,
+        typeMessage: result.typeMessage,
+        message: result.message,
+      };
+    }
+
     return await this.getListTodo();
   }
 
-  async addTodo(newTodo: NewTodoType) {
-    await this.addTodoSub.execute(this.listTodo, newTodo);
+  async addTodo(newTodo: NewTodoType): IReturnDefault {
+    const result = await this.addTodoSub.execute(
+      this.dataTodo.listTodo,
+      newTodo
+    );
+
+    if (result.typeMessage !== ITypeMessage.SUCCESS) {
+      return {
+        data: this.dataTodo,
+        typeMessage: result.typeMessage,
+        message: result.message,
+      };
+    }
+
     return await this.getListTodo();
   }
 
-  async removeTodo(id: string) {
-    await this.removeTodoSub.execute(id);
+  async removeTodo(id: string): IReturnDefault {
+    const result = await this.removeTodoSub.execute(id);
+
+    if (result.typeMessage !== ITypeMessage.SUCCESS) {
+      return {
+        data: this.dataTodo,
+        typeMessage: result.typeMessage,
+        message: result.message,
+      };
+    }
+
     return await this.getListTodo();
   }
 }
