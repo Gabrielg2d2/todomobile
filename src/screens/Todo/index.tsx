451a1cbb8fbb0d4483edb971b74205220667babd
Gabriel <gabriel.brito@acqio.com.br>
2024-07-03 08:16:00 -0300

    feat: new function handleResult template

--- src/screens/Todo/index.tsx
+++ src/screens/Todo/index.tsx
@@ -1,29 +1,36 @@
 import { useCallback, useEffect, useState } from "react";
 import { Alert } from "react-native";
-import { ITodoItem, NewTodoType, TodoMain } from "../../domain/todo/main";
+import { ITodoItem, NewTodoType } from "../../domain/todo/main";
 import { ITypeMessage } from "../../domainV2/global/types/typeMessage";
 import { TodoMainV2 } from "../../domainV2/todo/main";
 import TodoTemplate from "./todoTemplate";
 
+type IResult = {
+  data: {
+    listTodo: ITodoItem[];
+    allTodoCompleted: number;
+    quantityTodoCreated: number;
+  };
+  typeMessage: ITypeMessage;
+  message: string;
+};
+
 export function Todo() {
-  const [todoMain] = useState(new TodoMain());
-  const [todoMainV2] = useState(new TodoMainV2());
+  const [todoMain] = useState(new TodoMainV2());
   const [listTodo, setListTodo] = useState<ITodoItem[]>([]);
   const [informationTodos, setInformationTodos] = useState({
     quantityTodoCreated: 0,
     allTodoCompleted: 0,
   });
 
-  const getAllTodo = useCallback(async () => {
-    const result = await todoMainV2.getListTodo();
-
+  function handleResult(result: IResult) {
     if (result.typeMessage !== ITypeMessage.SUCCESS) {
       setListTodo([]);
       setInformationTodos({
         quantityTodoCreated: 0,
         allTodoCompleted: 0,
       });
-      Alert.alert("Erro", result.message);
+      if (result.message) Alert.alert("Erro", result.message);
       return;
     }
 
@@ -32,7 +39,23 @@ export function Todo() {
       quantityTodoCreated: result.data.quantityTodoCreated,
       allTodoCompleted: result.data.allTodoCompleted,
     });
+    if (result.message) Alert.alert("Sucesso", result.message);
+  }
+
+  const getAllTodo = useCallback(async () => {
+    const result = await todoMain.getListTodo();
+    handleResult(result);
   }, []);
+
+  async function handleAddTodo(newTodo: NewTodoType) {
+    const result = await todoMain.addTodo(newTodo);
+    handleResult(result);
+  }
+
+  async function handleToggleTodo(todo: ITodoItem) {
+    const result = await todoMain.toggleDone(todo);
+    handleResult(result);
+  }
 
   async function handleDeleteTodo(id: string) {
     Alert.alert(
@@ -48,41 +71,12 @@ export function Todo() {
           text: "Sim",
           onPress: async () => {
             const result = await todoMain.removeTodo(id);
-            if (result.success) {
-              return getAllTodo();
-            }
-
-            result.errorMessages.forEach((message) => {
-              Alert.alert("Erro", message);
-            });
+            handleResult(result);
           },
         },
       ],
       { cancelable: false }
     );
-  }
-
-  async function handleAddTodo(newTodo: NewTodoType) {
-    const result = await todoMain.addTodo(newTodo);
-    if (result.success) {
-      return getAllTodo();
-    }
-
-    result.errorMessages.forEach((message) => {
-      Alert.alert("Erro", message);
-    });
-  }
-
-  async function handleToggleTodo(todo: ITodoItem) {
-    const result = await todoMain.toggleDone(todo);
-
-    if (result.success) {
-      return getAllTodo();
-    }
-
-    result.errorMessages.forEach((message) => {
-      Alert.alert("Erro", message);
-    });
   }
 
   useEffect(() => {
